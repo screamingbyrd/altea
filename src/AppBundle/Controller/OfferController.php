971dd05c955daefb4b7c212a63273bd6daa53a00
screamingbyrd <regnault.arthur@gmail.com>
2018-11-20 12:43:49 +0100

    return

--- src/AppBundle/Controller/OfferController.php
+++ src/AppBundle/Controller/OfferController.php
@@ -317,7 +317,6 @@ class OfferController extends Controller
             $connection->rollback();
         }
 
-        return new Response();
         $files = glob( __DIR__ . '/../../../web/uploads/images/offer/*'); // get all file names
         foreach($files as $file){ // iterate files
             if(is_file($file))
